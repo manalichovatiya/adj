5e7a18a9e2bb1520a28b0405594d125728aae05b
manalichovatiya <115995019+manalichovatiya@users.noreply.github.com>
2023-03-17 17:27:55 +0530

    Add files via upload

---
+++ Filament-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char s1[1000],s2[1000];     
+    printf("Enter the string: ");
+    gets(s1);
+    strcpy(s2,s1);
+    strrev(s2);
+    int c;
+    c =strcmp(s1,s2);
+    if(c==0){
+    	printf("string is palindrome");
+	} 
+    else{
+    	printf("string is not palindrome");
+	}
+	
+    return 0;
+}
+
+
+
+
+ 
+
+
---
+++ Filament-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+#include <string.h> 
+int main(){
+ 	char h[1];
+  	printf("Enter the string: ");
+	scanf("%s",&h);
+
+	printf("%s",strlwr(h));  
+  return 0;
+}
+
+
